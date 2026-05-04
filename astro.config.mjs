32d546794f7c5d92836a066a4511d7fde68265e6
pickheaven <parktaesik119@gmail.com>
2026-05-04 21:44:56 +0900

    auto update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   // 본인의 도메인 주소로 수정하세요
-  site: 'https://pick79.com', 
+  site: 'https://pick79.com',
   integrations: [sitemap()],
 });
