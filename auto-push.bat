e4cf0e7ae4a30b4724c73f8f980bb415cd6aeb1a
pickheaven <parktaesik119@gmail.com>
2026-05-15 20:58:20 +0900

    auto update

--- auto-push.bat
+++ auto-push.bat
@@ -1,15 +1,15 @@
 @echo off
 cd /d %~dp0
 
-echo [1/3] 서버에서 최신 데이터 가져오는 중...
+echo [1/3] Retrieving latest DB...
 git pull origin main --rebase
 
-echo [2/3] 변경 사항 저장 중...
+echo [2/3] Saving changes...
 git add .
 git commit -m "auto update"
 
-echo [3/3] 깃허브로 푸쉬 중...
+echo [3/3] Pushing to GitHub...
 git push origin main
 
-echo 모든 작업이 완료되었습니다!
-pause
+echo All tasks completed!
+timeout /t 3
