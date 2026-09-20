8d7895bfd4d41f77ff9205f1164b4388913f6590
pickheaven <parktaesik119@gmail.com>
2026-09-20 18:00:31 +0900

    auto update

--- scripts/team_name_map.js
+++ scripts/team_name_map.js
@@ -785,7 +785,7 @@ const TEAM_NAME_MAP = {
   "Philadelphia Flyers": "필라델피아 플라이어스",
   "Pittsburgh Penguins": "피츠버그 펭귄스",
   "Tampa Bay Lightning": "탬파베이 라이트닝",
-  "Utah Mammoth": "유타 매머스",
+  "Utah Mammoth": "유타 매머드",
   "Vegas Golden Knights": "베가스 골든나이츠",
   "Toronto Maple Leafs": "토론토 메이플 리프스",
   "St. Louis Blues": "세인트루이스 블루스",
